c4bb06a8aa3538a8e456f162d434c18d6ba0e7b3
João Pedro Boldrini <103456597+Jeypas@users.noreply.github.com>
2023-08-07 18:00:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 function calcularSoma() {
   
-    const notap1 = parseFloat(document.getElementById("notap1").value);
-    const notap2 = parseFloat(document.getElementById("notap2").value);
-  
+    const notap1 = parseFloat(document.getElementById("notap1").value );
+    const notap2 = parseFloat(document.getElementById("notap2").value );
+   
 
     const resultado = (notap1 + (notap2 * 2)) / 3;
      
     const resultadoCerto = parseFloat(resultado.toFixed(2));
   
-    // Mostrar o resultado
     document.getElementById("resultado").innerText = ` ${resultado.toFixed(2)}`;
-  }
+    
+    document.getElementById("notap1").value = "";
+    document.getElementById("notap2").value = "";
+
+  const placeholder1 = document.getElementById("notap1");
+  const placeholder2 = document.getElementById("notap2");
+
+  if(resultado < 5){
+    placeholder1.placeholder = "Nota da P2";
+    placeholder2.placeholder = "Nota da P3";
+    }
+    else{
+      placeholder1.placeholder = "Nota da P1"
+      placeholder2.placeholder = "Nota da P2"
+    }
+  };
